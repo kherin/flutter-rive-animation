3533f581483e54cd1e679ed8fc2fe2e1feda4657
Kherin <kherin.j.s.bundhoo@gmail.com>
2022-07-02 11:13:31 +0400

    feat(animation): added loading animation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # flutter-rive-animation
 A simple project that showcases how to import an animation file created from Rive into your Flutter Project.
+
+## Dependencies
+- [rive](https://pub.dev/packages/rive): ^0.9.0 
+
+
+## References
+- [Rive App](https://rive.app/)
+- [Flutter Framework](https://flutter.dev/)
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Rive Animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SimpleAnimation(),
+    );
+  }
+}
+
+class SimpleAnimation extends StatelessWidget {
+  const SimpleAnimation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: RiveAnimation.asset('assets/animations/loading-animation.riv'),
+      ),
+    );
+  }
+}
